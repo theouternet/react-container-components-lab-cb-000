686bdc44cb115a9c57cb55caed56ae9515af5df9
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-08-29 22:37:11 +0000

    Automatically backed up by Learn

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -3,8 +3,8 @@ import 'isomorphic-fetch'
 import MovieReviews from './MovieReviews'
 
 const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ'
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
-            + `api-key=${NYT_API_KEY}`
+const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=';
+
             
 	export default class SearchableMovieReviewsContainer extends React.Component {
   constructor() {
