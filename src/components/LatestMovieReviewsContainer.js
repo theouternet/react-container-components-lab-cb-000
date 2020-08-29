fceda754e27a77164c719ef5b4b59741829e1f56
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-08-29 22:34:14 +0000

    Automatically backed up by Learn

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -6,4 +6,29 @@ const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code LatestMovieReviewsContainer Here
+
+
+
+export default class latestMovieReviewsContainer extends Component {
+  constructor() {
+    super()
+    this.state = {
+      reviews: []
+    }
+  }
+  
+  componentDidMount(){
+    fetch(URL)
+    .then(response => response.json())
+    .then(reviewsData => this.setState({reviews: reviewsData}))
+  }
+  
+  render(){
+    return(
+      <div className='latest-movie-reviews'>
+      <MovieReviews reviews={this.state.reviews} />
+      </div>
+      )
+  }
+  
+}
